239dc445e9b17053436f664f0d01662b403494d6
ZidanMhdvkia <mahdavikiazidan9@gmail.com>
2026-01-13 15:42:34 +0700

    migration/users and asset

---
+++ database/migrations/2026_01_13_082801_create_assets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->id('id_asset');
+            $table->string('bmn_code')->unique();
+            $table->string('device_name');
+            $table->enum('device_type', ['PC', 'Laptop']);
+            $table->string('gpu');
+            $table->year('procurement_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assets');
+    }
+};
